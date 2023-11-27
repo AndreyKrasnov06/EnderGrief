2852b40af6db1bb6e42d1dfee577c59f8cbf8580
Andrey <andrey.kr206@gmail.com>
2023-11-27 21:40:26 +0300

    initial commit

---
+++ src/main/java/com/voidminedevelopment/endergrief/EnderGrief.java
@@ -0,0 +1,38 @@
+package com.voidminedevelopment.endergrief;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnderGrief extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("EnderGrief - включён");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("EnderGrief - отключён");
+    }
+
+    @EventHandler
+    public void onEntityChangedBlock(EntityExplodeEvent event) {
+        if (event.getEntityType() == EntityType.ENDER_DRAGON) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onEndermanPickup(EntityChangeBlockEvent event) {
+        if (event.getEntityType() == EntityType.ENDERMAN) {
+            event.setCancelled(true);
+        }
+    }
+}
